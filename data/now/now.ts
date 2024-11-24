b2eae2831a96abcf046b2fc7a9381f2fb7ce4719
jaygal <joshuagalinato@gmail.com>
2024-11-24 11:16:08 +0100

    updated /now

--- data/now/now.ts
+++ data/now/now.ts
@@ -7,4 +7,8 @@ export default [
     desc: "I decided to put time aside and redo my portfolio. Not much has changed in terms of the UI, but I've been able to update the code as well as the tech to allow me to easily maintain and update it with ease.",
     date: "2024-10-22",
   },
+  {
+    desc: "It has been a while since I last did a 'Weekly Review', that's when I usually go through my files, clean things up and update my /now page. I need to get into the habit of doing this on a weekly basis.",
+    date: "2024-11-24",
+  },
 ];
