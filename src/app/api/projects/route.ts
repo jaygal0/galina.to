b3ff2407ebf86232a14b90761c9c2bd075b6842b
jaygal <joshuagalinato@gmail.com>
2026-01-31 09:41:57 +0100

    removed projects route

--- src/app/api/projects/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import projects from "../../../../data/home/projects";
-
-export async function GET() {
-  const data = await projects;
-  return NextResponse.json(data);
-}
